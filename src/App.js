e1d2a04d8ff95a3a806bf6c2534ddcfb0811ae9e
manoti <manotimike@gmail.com>
2023-05-01 10:21:03 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './Components/Header'
+import TeamsList from './Components/TeamsList'
+import AddTeam from './Components/AddTeam'
+import impfixtures from './data/teams'
+import { useState } from 'react'
+// console.log(impfixtures)
 
 function App() {
+  const [fixtures, setFixtures] = useState(impfixtures)
+
+  function addTeam(fixture) {
+    setFixtures((fixtures) => [...fixtures, fixture])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <AddTeam addTeam={addTeam} />
+      <TeamsList fixtures={fixtures} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
