927865f0b34aed00101582bd30fb5a8c360e8b98
manoti <manotimike@gmail.com>
2023-05-01 10:16:38 +0300

    added Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <React.Fragment>
+      <nav>
+        <div>
+          <img className='logo' src='shabana.png' alt='' />
+          <h1 id='titleHead'>Shabana FC</h1>
+          <img className='logo' id='nsl' src='nsl.jpeg' alt=''></img>
+        </div>
+      </nav>
+    </React.Fragment>
+  )
+}
+
+export default Header
