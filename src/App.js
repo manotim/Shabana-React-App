9c68888b7a374a854ab25559594d4e1a5eb39ecc
manoti <manotimike@gmail.com>
2023-05-02 10:58:47 +0300

    added fetch functionality

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import './App.css'
 import Header from './Components/Header'
 import TeamsList from './Components/TeamsList'
 import AddTeam from './Components/AddTeam'
-import impfixtures from './data/teams'
-import { useState } from 'react'
+// import impfixtures from './data/teams'
+import { useState, useEffect } from 'react'
 // console.log(impfixtures)
 
 function App() {
-  const [fixtures, setFixtures] = useState(impfixtures)
+  const [fixtures, setFixtures] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/fixtures')
+      .then((res) => res.json())
+      .then((data) => setFixtures(data))
+  }, [])
 
   function addTeam(fixture) {
+    fetch('http://localhost:3000/fixtures', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json',
+      },
+      body: JSON.stringify(fixture),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+
     setFixtures((fixtures) => [...fixtures, fixture])
   }
   return (
